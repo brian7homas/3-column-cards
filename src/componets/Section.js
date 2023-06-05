e1be5acf4979ad99628eb73efd9202d850c4913a
Brian Thomas <btdeveloper125@gmail.com>
2023-06-05 10:28:54 -0600

    refactor(Section-1): BASE CSS
    main css for all 3 sections

--- src/componets/Section.js
+++ src/componets/Section.js
@@ -6,7 +6,13 @@ function Section(props) {
       className="section section--outer"
       style={{'backgroundColor': `${props.bgColor}`}}
       >
+      <div>{props.icon}</div>
+      <h1 className="section--vehicle">{props.vehicle}</h1>
+      <p className="section--copy">{props.copy}</p>
+      <button 
+        className="section--button"
         style={{color: `${props.bgColor}`}}
+      >Learn More</button>
     </section>
   )
 }
